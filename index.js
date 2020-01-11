dd177a91c06ee0704e38cb10d683f0e9999da227
Eli Kasik <soujiro32167@gmail.com>
2020-01-10 23:24:23 -0500

    one by one

--- index.js
+++ index.js
@@ -3,25 +3,34 @@ const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 const scraper = require('./scraper')
 const { range } = require('./polyfill')
+const fs = require('fs')
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0'
 
 const getPage = (url) => fetch(url)
-        .then(res => res.text())
+        .then(res => {
+            if (!res.ok) { throw `response: ${res.status}` }; 
+            return res.text()
+        })
         .then(text => new JSDOM(text))
 
 const baseUri = 'https://database.globalreporting.org/organizations'
 
-function main(){
-    return Promise.all(
-        range(10, 14).xs()
-        .map(n => getPage(`${baseUri}/${n}`))
-        .map(domPromise => domPromise
-                .then(dom => scraper.company(dom.window.document, dom.window))
-                .then(result => console.log(JSON.stringify(result, null, 2)))
-                .catch(e => console.error(e))
-            )
-    )
+async function main(){
+    const results = []
+    for (let n of range(10, 100)){
+        try {
+            const dom = await getPage(`${baseUri}/${n}`)
+            const result = scraper.company(dom.window.document, dom.window)
+            console.log(`parsed page ${n}`)
+            results.push([n, result])
+        } catch(e) {
+            console.error(e)
+        }
+    }
+    
+    // console.log(results)
+    fs.writeFileSync('results.json', JSON.stringify(Object.fromEntries(results)))
 }
 
 main()
